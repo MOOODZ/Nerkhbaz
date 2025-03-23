011681e6ce5ad35695ed6a5ef061d959e5ba43c9
Mohamadreza Mehrjou <mohamadmehrjou@gmail.com>
2025-03-23 22:49:32 +0330

    Initial commit

---
+++ app/src/main/java/ir/moodz/sarafkoochooloo/domain/util/DataError.kt
@@ -0,0 +1,21 @@
+package ir.moodz.sarafkoochooloo.domain.util
+
+import ir.moodz.core.domain.util.Error
+
+sealed interface DataError: Error {
+    enum class Network: DataError{
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        CONFLICT,
+        TOO_MANY_REQUEST,
+        NO_INTERNET,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError{
+        DISK_FULL
+    }
+}
---
+++ app/src/main/java/ir/moodz/sarafkoochooloo/domain/util/Error.kt
@@ -0,0 +1,3 @@
+package ir.moodz.core.domain.util
+
+interface Error
